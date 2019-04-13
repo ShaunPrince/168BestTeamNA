50cf0360daadcfb393c0d2e6eb89fa9c535ad651
Shaun Prince <prince.a.shaun@gmail.com>
2019-04-13 14:48:31 -0700

    Added additionaly organizer folders, added Bullet and astroid prefabs, setup additional layers for physics collisions

---
+++ 168BestTeamNA/Assets/_Scripts/DamagableEntity.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagableEntity : MonoBehaviour
+{
+    //EColor will be used for identification and damage
+    public enum EColor {Red, Blue, Green, Yellow, Gray};
+
+    public EColor curColor;
+    public float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Bullet incomingBullet = collision.gameObject.GetComponent<Bullet>();
+        if (incomingBullet != null)
+        {
+            this.TakeDamage(incomingBullet.dmg);
+            Destroy(incomingBullet);
+        }
+    }
+
+    public virtual void TakeDamage(float dmg)
+    {
+        this.health -= dmg;
+        if (this.health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
