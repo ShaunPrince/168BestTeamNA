b476335e41c438d39c2eb034ca7fd81566911487
aderevlany <avaderevlany@gmail.com>
2019-05-02 12:54:54 -0700

    Gameover UI working now, yay

---
+++ 168BestTeamNA/Assets/_Scripts/GameOverUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverUI : MonoBehaviour
+{
+    public GameObject gameOverUI;
+    //public GameObject floor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOverUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject tempFloor = GameObject.Find("Floor");
+        if (tempFloor == null)
+        {
+            gameOverUI.SetActive(true);
+        }
+    }
+}
